86890494194977f92526f1da98aedf6a64366288
Hugo Piat <h.piat@outlook.fr>
2020-11-13 00:31:46 +0100

    Fixing initialization logic

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Dimensions } from 'react-native';
 
 const EMPTY_CASE = 0;
@@ -10,6 +10,7 @@ const BRICK_IMAGE = '../assets/brick_wall.png';
 interface IProps {
   start: [number, number];
   end: [number, number];
+  player: [number, number];
   blocks: [number, number][];
 }
 
@@ -22,30 +23,32 @@ export default function Grid({
   const height = Dimensions.get('window').height;
   const [grid, setGrid] = useState<number[][]>(Array(width / CASE_SIZE));
 
+  const isStart = useCallback((i: number, j: number) => start[0] === i && start[1] === j, []);
+  const isEnd = useCallback((i: number, j: number) => end[0] === i && end[1] === j, []);
+  const hasBlock = useCallback((i: number, j: number) => {
+    const hasBlock = blocks.some(b => b[0] !== i && b[1] !== j) ||
+      i === 0 ||
+      i === grid.length - 1 ||
+      j === 0 ||
+      j === grid[i].length - 1;
+    return !isStart(i, j) && !isEnd(i, j) && hasBlock;
+  }, []);
+
   useEffect(() => {
     for (let i = 0; i < grid.length; i++) {
       setGrid(grid => {
         grid[i] = Array(height / CASE_SIZE);
-        const shouldBlock = (i: number, j: number) => {
-          const isStart = start[0] === i && start[1] === j;
-          const isEnd = end[0] === i && end[1] === j;
-          const hasBlock = blocks.some(b => b[0] !== i && b[1] !== j) ||
-            i === 0 ||
-            i === grid.length - 1 ||
-            j === 0 ||
-            j === grid[i].length - 1;
-          return !isStart && !isEnd && hasBlock;
-        };
         for (let j = 0; j < grid[i].length; j++) {
-          if (shouldBlock(i, j)) {
-            grid[i][j] = 1;
+          if (hasBlock(i, j)) {
+            grid[i][j] = BRICK_CASE;
+          } else if (isStart(i, j)) {
+            grid[i][j] = PLAYER_CASE;
           } else {
-            grid[i][j] = 0;
+            grid[i][j] = EMPTY_CASE;
           }
         }
         return grid;
       })
     }
   }, []);
-
 }
