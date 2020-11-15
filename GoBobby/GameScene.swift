beb8350cf0fb0f6888bd12a53242db76ed90cf93
Hugo Piat <h.piat@outlook.fr>
2020-11-15 22:09:14 +0100

    Setup refacto Swift

---
+++ GoBobby/GameScene.swift
@@ -0,0 +1,29 @@
+//
+//  GameScene.swift
+//  GoBobby
+//
+//  Created by Hugo Piat on 15/11/2020.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        let wall = SKSpriteNode(texture: SKTexture(imageNamed: "brick_wall.png"))
+        wall.name = "wall"
+        wall.position.x = 10
+        wall.position.y = 10
+        wall.zPosition = 1
+        self.addChild(wall)
+        
+        let wall2 = SKSpriteNode(texture: SKTexture(imageNamed: "brick_wall.png"))
+        wall.name = "wall2"
+        wall.position.x = 20
+        wall.position.y = 20
+        wall.zPosition = 1
+        self.addChild(wall2)
+    }
+}
--- components/utils/math.ts
+++
@@ -1,5 +0,0 @@
-export const doubleEquality = (i: number, j: number, entity: [number, number]) =>
-  !!entity && entity[0] === i && entity[1] === j;
-
-export const objectsDoubleEquality = (first: [number, number], other: [number, number]) =>
-  !!first && !!other && first[0] === other[0] && first[1] === other[1];
