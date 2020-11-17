edd57e4e10ebddc85785a4c907a6aba636af6a59
Hugo Piat <h.piat@outlook.fr>
2020-11-17 17:04:53 +0100

    Adding level 9

---
+++ GoBobby/Levels/L9.swift
@@ -0,0 +1,28 @@
+//
+//  L9.swift
+//  GoBobby
+//
+//  Created by Hugo Piat on 17/11/2020.
+//
+
+import Foundation
+
+public struct L9 : LevelProtocol {
+    var grid: Grid
+    var player: Player
+    let blocks: [Block]
+    let end: Block
+    
+    init() {
+        player = Player(X: 10, Y: 17, orientation: Orientation.down)
+        blocks = [Block(X: 6, Y: 6), Block(X: 2, Y: 2), Block(X: 11, Y: 8), Block(X: 11, Y: 10),
+                  Block(X: 1, Y: 17), Block(X: 7, Y: 18), Block(X: 10, Y: 11), Block(X: 9, Y: 13),
+                  Block(X: 7, Y: 8), Block(X: 7, Y: 9), Block(X: 7, Y: 10), Block(X: 9, Y: 11),
+                  Block(X: 1, Y: 4), Block(X: 4, Y: 9), Block(X: 3, Y: 12), Block(X: 8, Y: 11),
+                  Block(X: 3, Y: 4), Block(X: 7, Y: 2), Block(X: 9, Y: 7), Block(X: 9, Y: 11),
+                  Block(X: 3, Y: 8), Block(X: 11, Y: 11), Block(X: 9, Y: 12), Block(X: 1, Y: 4),
+                  Block(X: 12, Y: 8), Block(X: 12, Y: 9), Block(X: 5, Y: 1)]
+        end = Block(X: 6, Y: 0)
+        grid = Grid(player: player, blocks: blocks, end: end)
+    }
+}
