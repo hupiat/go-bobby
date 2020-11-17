687216a71171c983251e8687c31dd414473169f0
Hugo Piat <h.piat@outlook.fr>
2020-11-17 16:29:07 +0100

    Adding level 7

---
+++ GoBobby/Levels/L7.swift
@@ -0,0 +1,25 @@
+//
+//  L7.swift
+//  GoBobby
+//
+//  Created by Hugo Piat on 17/11/2020.
+//
+
+import Foundation
+
+public struct L7 : LevelProtocol {
+    var grid: Grid
+    var player: Player
+    let blocks: [Block]
+    let end: Block
+    
+    init() {
+        player = Player(X: 10, Y: 9, orientation: Orientation.left)
+        blocks = [Block(X: 3, Y: 3), Block(X: 2, Y: 2), Block(X: 11, Y: 8), Block(X: 10, Y: 10),
+                  Block(X: 1, Y: 17), Block(X: 7, Y: 18), Block(X: 9, Y: 18), Block(X: 9, Y: 13),
+                  Block(X: 2, Y: 11), Block(X: 7, Y: 2), Block(X: 9, Y: 8), Block(X: 9, Y: 11),
+                  Block(X: 3, Y: 8), Block(X: 11, Y: 11), Block(X: 11, Y: 11), Block(X: 1, Y: 4)]
+        end = Block(X: 10, Y: 19)
+        grid = Grid(player: player, blocks: blocks, end: end)
+    }
+}
