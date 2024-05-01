736e5d20a1d7a9b02aef3737f80169506624b943
Hugo Piat <h.piat@outlook.fr>
2024-05-01 17:50:08 +0200

    src/ struct & updated package.json

--- GoBobby/index.js
+++ GoBobby/index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import {registerRootComponent} from 'expo';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
