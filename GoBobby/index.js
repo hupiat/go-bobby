61831963b7d62e26dc7bf1fb57f6f8d610bbacdc
Hugo Piat <h.piat@outlook.fr>
2024-05-01 17:23:46 +0200

    setup working properly

---
+++ GoBobby/index.js
@@ -0,0 +1,14 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {registerRootComponent} from 'expo';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+
+// Needed for expo
+// This may be commented in production
+registerRootComponent(App);
